4b098b2fc7013f7e17e0620908463443d8fac065
elyset <elysetoder@gmail.com>
2022-10-13 22:48:27 -0400

    b

--- Lab 4/joystick_show_image.py
+++ Lab 4/joystick_show_image.py
@@ -122,7 +122,7 @@ backlight.value = True
 text_image = Image.new("RGB", (width, height))
 text_draw = ImageDraw.Draw(text_image)
 text_draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-disp.image(text_image, 90)
+disp.image(text_image)
 
 
 # Scale the image to the smaller screen dimension
@@ -165,7 +165,7 @@ while True:
     text_draw.text((0, 50), display_text, font=font, fill="#FF00FF")
 
     # Display image.
-    disp.image(text_image)
+    disp.image(text_image, 90)
 
     if has_moved_left(myJoystick):
         disp.image(image)
