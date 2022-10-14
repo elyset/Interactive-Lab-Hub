54d42f25e8733297180af0f49b96a91cb894a897
elyset <elysetoder@gmail.com>
2022-10-13 23:25:20 -0400

    new pic

--- Lab 4/pls.py
+++ Lab 4/pls.py
@@ -76,24 +76,23 @@ def has_moved_left(joystick):
 
 
 def display_dog_bone():
-    image = Image.open("dog_bone3.jpeg")
-    image_ratio = image.height / image.width
+    image = Image.open("dog_bone4.jpeg")
+    image_ratio = image.width / image.height
 
-    screen_ratio = height / width
+    screen_ratio = width / height
     if screen_ratio < image_ratio:
-        scaled_width = image.height * width // image.width
-        scaled_height = width
+        scaled_width = image.width * height // image.height
+        scaled_height = height
     else:
-        scaled_width = height
-        scaled_height = image.width * height // image.height
+        scaled_width = width
+        scaled_height = image.height * width // image.width
     image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
 
     # Crop and center the image
-    x = scaled_width // 2 - height // 2
-    y = scaled_height // 2 - width // 2
-    image = image.crop((y, x, y + height, x + width))
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
 
-    print(image.width, image.height)
     # Display image.
     disp.image(image)
 
