097fb235e8906dbed70c2c98164ba029e946af0e
elyset <elysetoder@gmail.com>
2022-10-13 23:33:44 -0400

    new bone

--- Lab 4/pls.py
+++ Lab 4/pls.py
@@ -76,7 +76,7 @@ def has_moved_left(joystick):
 
 
 def display_dog_bone():
-    image = Image.open("dog_bone5.png")
+    image = Image.open("dog_bone6.jpeg")
     image_ratio = image.width / image.height
 
     screen_ratio = width / height
