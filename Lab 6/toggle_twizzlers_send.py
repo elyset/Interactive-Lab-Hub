31aff5c598fd133a764687555bf612a65366c07c
elyset <elysetoder@gmail.com>
2022-11-03 15:58:26 -0400

    fix stuff

--- Lab 6/toggle_twizzlers_send.py
+++ Lab 6/toggle_twizzlers_send.py
@@ -18,7 +18,7 @@ client.connect(
 topic = 'IDD/your/topic/here'
 
 #i2c = busio.I2C(board.SCL, board.SDA)
-myJoystick = qwiic_joystick.QwiicJoystick()
+joystick = qwiic_joystick.QwiicJoystick()
 
 #mpr121 = adafruit_mpr121.MPR121(i2c)
 
