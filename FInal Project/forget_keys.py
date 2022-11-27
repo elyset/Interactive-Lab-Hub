9c818b53c0d62608980794ea3cb0577329116163
elyset <elysetoder@gmail.com>
2022-11-27 14:40:00 -0500

    keys

---
+++ FInal Project/forget_keys.py
@@ -0,0 +1,31 @@
+import time
+import board
+import busio
+import adafruit_mpr121
+
+import paho.mqtt.client as mqtt
+import uuid
+
+client = mqtt.Client(str(uuid.uuid1()))
+client.tls_set()
+client.username_pw_set('idd', 'device@theFarm')
+
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+#topic = 'IDD/your/topic/here'
+topic = 'IDD/Leaving house checklist'
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+mpr121 = adafruit_mpr121.MPR121(i2c)
+
+while True:
+    for i in range(12):
+        if mpr121[i].value:
+            if i == 7:
+                val = f"Don't forget to bring your keys!"
+                print(val)
+                client.publish(topic, val)
+    time.sleep(0.25)
