fb112ec75f746dfeb09dae125edc319c4c933a02
elyset <elysetoder@gmail.com>
2022-11-29 22:31:04 -0500

    fixed message

--- FInal Project/forget_keys.py
+++ FInal Project/forget_keys.py
@@ -8,14 +8,22 @@ import uuid
 
 from twilio.rest import Client
 
-TWILIO_ACCOUNT_SID = '****' # Account SID
-TWILIO_AUTH_TOKEN = '****' # Auth Token
+TWILIO_ACCOUNT_SID = 'ACd96e8a3f32c9309ab7e87a27e332804b' # Account SID
+TWILIO_AUTH_TOKEN = '37f90d364e158015b96c7ad0bca462fd' # Auth Token
 TWILIO_PHONE_SENDER = "8656013981" # phone number registered in twilio
 TWILIO_PHONE_RECIPIENT = "9173186696" # my phone number
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
 mpr121 = adafruit_mpr121.MPR121(i2c)
+
+body = '''
+Hey Elyse! Here is your checklist for leaving the house:
+✓ Keys
+✓ Wallet
+✓ Water for dog
+✓ Work laptop
+'''
 
 while True:
     for i in range(12):
@@ -25,6 +33,7 @@ while True:
                 message = client.messages.create(
                     to=TWILIO_PHONE_RECIPIENT,
                     from_=TWILIO_PHONE_SENDER,
-                    body="test")
+                    body=body)
                 print(message.sid) 
+
 
