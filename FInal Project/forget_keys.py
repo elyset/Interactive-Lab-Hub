682c188377c67853b58bb9a85c9ae39bb0ba2518
elyset <elysetoder@gmail.com>
2022-12-02 19:18:59 -0500

    new shoes

--- FInal Project/forget_keys.py
+++ FInal Project/forget_keys.py
@@ -18,18 +18,34 @@ i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
 body = '''
-Hey Elyse! Here is your checklist for leaving the house:
+Hey Elyse! Here is your 💼 work 💼 checklist for leaving the house:
 ✓ Keys
 ✓ Wallet
-✓ Water for dog
+✓ Add Water in Dog Bowl
 ✓ Work laptop
+✓ Lunch
 '''
 body2 = '''
-Hey Elyse! Here is your running 🏃‍♀️ checklist for leaving the house:
+Hey Elyse! Here is your 🏃‍♀️ running 🏃‍♀️ checklist for leaving the house:
 ✓ Keys
 ✓ Wallet
 ✓ Water bottle
 ✓ Earphones
+'''
+body3 = '''
+Hey Elyse! Here is your 🐕 walking dog 🐕 checklist for leaving the house:
+✓ Keys
+✓ Wallet
+✓ Dog Water Bottle
+✓ Treats
+✓ Tennis Ball
+'''
+body4 = '''
+Hey Elyse! Here is your 👠 fancy dinner 👠 checklist for leaving the house:
+✓ Keys
+✓ Wallet
+✓ Lipstick
+✓ Add Water in Dog Bowl
 '''
 
 while True:
@@ -48,6 +64,20 @@ while True:
                     to=TWILIO_PHONE_RECIPIENT,
                     from_=TWILIO_PHONE_SENDER,
                     body=body2)
-                print(message.sid) 
+                print(message.sid)
+            if i == 4:
+                client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+                message = client.messages.create(
+                    to=TWILIO_PHONE_RECIPIENT,
+                    from_=TWILIO_PHONE_SENDER,
+                    body=body3)
+                print(message.sid)
+            if i == 11:
+                client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+                message = client.messages.create(
+                    to=TWILIO_PHONE_RECIPIENT,
+                    from_=TWILIO_PHONE_SENDER,
+                    body=body4)
+                print(message.sid)    
 
 
