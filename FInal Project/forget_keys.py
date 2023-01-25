4c1820d8ef74ce96fd82502d910574008b9372d3
elyset <61363525+elyset@users.noreply.github.com>
2023-01-24 19:20:21 -0500

    Update forget_keys.py

--- FInal Project/forget_keys.py
+++ FInal Project/forget_keys.py
@@ -10,8 +10,8 @@ from twilio.rest import Client
 
 TWILIO_ACCOUNT_SID = 'ACd96e8a3f32c9309ab7e87a27e332804b' # Account SID
 TWILIO_AUTH_TOKEN = '37f90d364e158015b96c7ad0bca462fd' # Auth Token
-TWILIO_PHONE_SENDER = "8656013981" # phone number registered in twilio
-TWILIO_PHONE_RECIPIENT = "9173186696" # my phone number
+TWILIO_PHONE_SENDER = "XXXX" # phone number registered in twilio
+TWILIO_PHONE_RECIPIENT = "XXXX" # my phone number
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
